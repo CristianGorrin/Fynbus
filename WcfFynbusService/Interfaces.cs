d0c6479387014a03ac320ba24190a46ceaeb3a0f
Cristian C. Gorrin <cristian_gorrin@live.com>
2015-04-13 15:01:03 +0200

    Moved EF to the WCF

---
+++ WcfFynbusService/Interfaces.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfFynbusService.Interfaces
+{
+    [ServiceContract]
+    public interface ILogin
+    {
+        [OperationContract]
+        string LoginServices(string acc, string password);
+
+        [OperationContract]
+        bool ValidateToken(string token, string acc);
+    }
+}
---
+++ WcfFynbusService/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfFynbusService
+{
+    public class Login : Interfaces.ILogin
+    {
+        public string LoginServices(string acc, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateToken(string token, string acc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
