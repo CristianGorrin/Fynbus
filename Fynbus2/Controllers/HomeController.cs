50d0d47dbdc086ebd0b7817da5b77c48133d7948
FSKjeldsen <frederik@feedex.dk>
2015-04-22 17:17:17 +0200

    Replaced website project with updated version

---
+++ Fynbus2/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fynbus2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        public ActionResult Vehicles()
+        {
+            return View();
+        }
+
+        public ActionResult Offers()
+        {
+            return View();
+        }
+
+        public ActionResult AddVehicle()
+        {
+            return View();
+        }
+
+        public ActionResult AddOffer()
+        {
+            return View();
+        }
+
+        public ActionResult EditVehicle(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
